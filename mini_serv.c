a4070f2115a5e3274a9c1e60e886610a31be3717
Daniel Exposito lopez <dexposit@c1r10s1.42madrid.com>
2024-11-17 20:49:35 +0100

    starting trying to disconect message but sometime is wrong with the FD handling

--- mini_serv.c
+++ mini_serv.c
@@ -4,6 +4,9 @@
 #include <netinet/in.h>
 #include <string.h>
 #include <stdlib.h>
+#include <stdio.h>
+
+#define BUFFLEN 10000
 
 void leaks(void)
 {
@@ -18,10 +21,17 @@ void printError(char *str)
 	write(2, "\n", 1);
 	exit(1);
 }
+typedef struct s_client
+{
+	int	id;
+	char	msg[BUFFLEN];
+}	t_client;
+
+t_client client[1024];
 
 int main(int argc, char **argv)
 {
-	int fds;
+	int fds, clients;
 	atexit(leaks);
 	if (argc != 2)
 	{
@@ -57,13 +67,15 @@ int main(int argc, char **argv)
 	//Preparar conexión de un único cliente
 	struct sockaddr_in cliaddr;
 	int lencli = sizeof(cliaddr);
-	char buffer[10000];
+	char buffer[BUFFLEN];
+	clients = 0;
 
 	while (1)
 	{
 		setRead = setWrite = setStatus;	
 		if (0 > select(fds + 1, &setRead, &setWrite, 0, 0))
-			printError(NULL);
+			continue ;
+			//printError(NULL);
 		for (int id = 0; id <= fds; id++)
 		{
 			//Comprobar Si el fd esta en lectura o escritura
@@ -75,23 +87,75 @@ int main(int argc, char **argv)
 					//Aceptamos la primera conexión de un cliente y creamos su respectivo socket ...
 					int fd_client = accept(fd_socket, (struct sockaddr *) &cliaddr, &lencli);
 					if (0 > fd_client)
-						printError(NULL);
+						continue ;
+					//	printError(NULL);
 					//Actualizar max fd con el nuevo creado (cuando se desconecten revisar si hay más que antes o no
-					fds = fd_client;
+					if (fds < fd_client)
+						fds = fd_client;
 					//Añadir nuevo fd al select
 					FD_SET(fd_client, &setStatus);
+					client[fd_client].id = clients++;
+					sprintf(buffer, "server: client %d just arrived\n", client[fd_client].id);
 					//Cambiar por mensaje a todos los clientes
 					write(1, "Conectado\n", 10);
+					for (int i = 0; i < fds; i++)
+					{
+						if (i != fd_client && FD_ISSET(i, &setWrite))
+						{
+							if (0 > send(i, buffer, strlen(buffer), 0))
+								printError(NULL);
+						}
+					}
 					break ;
 				}
 				else
 				{
 					//Algún cliente evento de lectura por parte del serv
 					//Server espera bloqueante un mensaje del fd_client que le digamos en este caso el único cliente que se puede conectar
-					int msg_len = recv(id, &buffer, 10000, 0);
+					int msg_len = recv(id, &buffer, BUFFLEN, 0);
 					//if msg_len < 0 handler client desconection
-					//Enviar mensaje en el buffer al resto de clientes conectados
-					write(1, buffer, msg_len);
+					//Envia mensaje en el buffer al resto de clientes conectados
+					if (msg_len > 0)
+					{
+						//Recortar mensaje por líneas
+						//Añadir mensaje al principio dela línea y enviar
+//						repetir hasta que no queden líneas
+/*
+						int start, end;
+						start = end = 0;
+						do {
+							sprintf(buffer, "client: %d: ", clients[id].id);
+							while (client[id].msg[end] != '\n')
+								end++;
+							
+							sprintf(client[id].msg, "client: %d: %s\n", clients[id].id,
+						} while (start < strlen(buffer));
+*/
+						for (int i = 0; i < fds; i++)
+						{
+							if (i != id && FD_ISSET(i, &setWrite))
+							{
+								if (0 > send(i, buffer, strlen(buffer), 0))
+									printError(NULL);
+							}
+						}
+						write(1, buffer, msg_len);
+					}
+					else
+					{
+						sprintf(buffer, "server: client %d just left\n", client[id].id);
+						for (int i = 0; i < fds; i++)
+						{
+							if (i != id && FD_ISSET(i, &setWrite))
+							{
+								if (0 > send(i, buffer, strlen(buffer), 0))
+									printError(NULL);
+							}
+						}
+						write(1, "Desconectado\n", 13);
+						FD_CLR(id, &setStatus);
+						close(id);
+					}
 				}
 			}
 		}
